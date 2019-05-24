55b981f0d5f415573b428cfeae6f4f5b1bbf3feb
KOLESNIKOV Vladislav <Vladislav.KOLESNIKOV@raiffeisen.ru>
2019-05-24 23:09:41 +0300

    added age stt

--- src/AppMain/PageHolder/pages/AgeStatistic.tsx
+++ src/AppMain/PageHolder/pages/AgeStatistic.tsx
@@ -1,15 +1,63 @@
 import React from 'react';
+import axios from 'axios'
+
+type Column = {
+    department: string,
+    under18: string,
+    age18_21: string,
+    age22_24: string,
+    age25_29: string,
+    age30_34: string,
+    age35_39: string,
+    age40_44: string,
+    age45_49: string,
+    age50_54: string,
+    age55_59: string,
+    age60_64: string,
+    age65_69: string,
+    age70_74: string,
+    age75: string,
+}
+
+type TState = {
+    now: string,
+    data: Column [],
+}
 
 
-class AgeStatistic extends React.Component<{}, {}> {
+class AgeStatistic extends React.Component<{}, TState> {
     public state = {
-        now: ""
-    }
+        now: "123",
+        data: []
+    };
 
-    render(){
+    sendReq = (): void => {
+        axios({
+            method: 'post',
+            url: 'http://localhost:3001/ageStatistic/',
+        })
+            .then((response) => {
+                console.log(response.data);
 
-        return(
-            <div>AgeStatistic</div>
+                this.setState({
+                    data: response.data
+                })
+            })
+            .catch(function (error) {
+                alert(error);
+            });
+
+
+    };
+
+
+    render() {
+        const {data} = this.state;
+        return (
+            <div>AgeStatistic
+                <button onClick={this.sendReq}>click</button>
+                {data}
+            </div>
         );
     }
 
